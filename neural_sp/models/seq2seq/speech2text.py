afffcfb3c9d95d0322e2bfe98272616574b0f64f
iwaterxt <iwaterxt@gmail.com>
2019-08-21 17:52:02 +0800

    support parallel

--- neural_sp/models/seq2seq/speech2text.py
+++ neural_sp/models/seq2seq/speech2text.py
@@ -514,12 +514,9 @@ class Speech2Text(ModelBase):
             # Flip acoustic features in the reverse order
             if flip:
                 xs = [torch.from_numpy(np.flip(x, axis=0).copy()).float().cuda(self.device_id) for x in xs]
-            else:
-                print (xs.size)
-                print (xlens.size)
-                pass                
+            else:           
                 #xs = [np2tensor(x, self.device_id).float() for x in xs]
-            #xs = pad_list(xs, 0.0)
+                xs = pad_list(xs, 0.0)
 
             # SpecAugment
             if self.is_specaug and self.training:
