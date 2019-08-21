ac05eb7d233b2fb9057918bb5838d6a4f54aef80
iwaterxt <iwaterxt@gmail.com>
2019-08-21 16:42:20 +0800

    support parallel

--- neural_sp/datasets/asr.py
+++ neural_sp/datasets/asr.py
@@ -50,14 +50,14 @@ def pad_list(xs, value, pad_left=False):
         if pad_left:
             if item.shape(1) != max_time:
                 mat = np.zeros(shape(0), max_time)
-                mat[,-item.shape(1):] = item
+                mat[:,-item.shape(1):] = item
                 xs_pad.append(mat)
             else:
                 xs_pad.append(item)
         else:
             if item.shape(1) != max_time:
                 mat = np.zeros(shape(0), max_time)
-                mat[,:item.shape(1)] = item
+                mat[:,:item.shape(1)] = item
             else:
                 xs_pad.append(item)
 
