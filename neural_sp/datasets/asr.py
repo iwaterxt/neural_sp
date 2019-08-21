51cd6a6fa2f934782ad705789aa3046826dc5675
iwaterxt <iwaterxt@gmail.com>
2019-08-21 16:46:05 +0800

    support parallel

--- neural_sp/datasets/asr.py
+++ neural_sp/datasets/asr.py
@@ -44,20 +44,20 @@ def count_vocab_size(dict_path):
 
 def pad_list(xs, value, pad_left=False):
     num = len(xs)
-    max_time = max(x.shape(1) for x in xs)
+    max_time = max(x.shape[1] for x in xs)
     xs_pad=[]
     for item in xs:
         if pad_left:
-            if item.shape(1) != max_time:
-                mat = np.zeros(shape(0), max_time)
-                mat[:,-item.shape(1):] = item
+            if item.shape[1] != max_time:
+                mat = np.zeros(shape[0], max_time)
+                mat[:,-item.shape[1]:] = item
                 xs_pad.append(mat)
             else:
                 xs_pad.append(item)
         else:
-            if item.shape(1) != max_time:
-                mat = np.zeros(shape(0), max_time)
-                mat[:,:item.shape(1)] = item
+            if item.shape[1] != max_time:
+                mat = np.zeros(shape[0], max_time)
+                mat[:,:item.shape[1]] = item
             else:
                 xs_pad.append(item)
 
