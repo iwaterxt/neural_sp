9330a01cc135ee2a5144dbcc866a3364500edaad
iwaterxt <iwaterxt@gmail.com>
2019-08-21 11:11:24 +0800

    support parallel

--- neural_sp/bin/asr/train_horovod.py
+++ neural_sp/bin/asr/train_horovod.py
@@ -291,19 +291,6 @@ def main():
         # Set optimizer
         optimizer = set_optimizer(model, args.optimizer, args.lr, args.weight_decay)
 
-        # Wrap optimizer by learning rate scheduler
-        noam = 'transformer' in args.enc_type or args.dec_type == 'transformer'
-        optimizer = LRScheduler(optimizer, args.lr,
-                                decay_type=args.lr_decay_type,
-                                decay_start_epoch=args.lr_decay_start_epoch,
-                                decay_rate=args.lr_decay_rate,
-                                decay_patient_n_epochs=args.lr_decay_patient_n_epochs,
-                                early_stop_patient_n_epochs=args.early_stop_patient_n_epochs,
-                                warmup_start_lr=args.warmup_start_lr,
-                                warmup_n_steps=args.warmup_n_steps,
-                                model_size=args.d_model,
-                                factor=args.lr_factor,
-                                noam=noam)
 
         hvd.broadcast_parameters(model.state_dict(), root_rank=0)
         hvd.broadcast_optimizer_state(optimizer, root_rank=0)
