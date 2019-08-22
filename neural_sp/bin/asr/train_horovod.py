d2c179c63c9cd8e47c7d2185fed079bd835397e1
iwaterxt <iwaterxt@gmail.com>
2019-08-22 11:12:07 +0800

    update

--- neural_sp/bin/asr/train_horovod.py
+++ neural_sp/bin/asr/train_horovod.py
@@ -308,8 +308,8 @@ def main():
         # Compute loss in the training set
         for i, batch_train in enumerate(train_loader):
             accum_n_tokens += sum([len(y) for y in batch_train['ys']])
-            print(len(batch_train['xs']))
-            print(batch_train['xs'][0])
+            batch_train['xs'] = [tensor2np(x).float() for x in batch_train['xs']]
+
             # Change mini-batch depending on task
             for task in tasks:
                 if skip_thought:
