ee31a19f52d72d806524e8053e2c2fde43f44409
iwaterxt <iwaterxt@gmail.com>
2019-08-22 11:13:20 +0800

    update

--- neural_sp/models/seq2seq/speech2text.py
+++ neural_sp/models/seq2seq/speech2text.py
@@ -516,8 +516,7 @@ class Speech2Text(ModelBase):
                 xs = [torch.from_numpy(np.flip(x, axis=0).copy()).float().cuda(self.device_id) for x in xs]
             else:           
                 xs = [np2tensor(x, self.device_id).float() for x in xs]
-                #xs = [x for x in xs]
-                #xs = pad_list(xs, 0.0)
+                xs = pad_list(xs, 0.0)
 
             # SpecAugment
             if self.is_specaug and self.training:
