1b780e2db72e6311b2f64df95a239a652646c6b4
iwaterxt <iwaterxt@gmail.com>
2019-08-28 00:09:24 +0800

    update

--- neural_sp/bin/asr/train_horovod.py
+++ neural_sp/bin/asr/train_horovod.py
@@ -155,12 +155,12 @@ def main():
     # `num_replicas=hvd.size()` and `rank=hvd.rank()`.
     train_loader = SeqDataloader(train_set, 
                                  batch_size=args.batch_size,
-                                 num_workers = 2,
+                                 num_workers = 4,
                                  distributed=True
                                 )
     val_loader = SeqDataloader(dev_set, 
                                batch_size=args.batch_size,
-                               num_workers = 1,
+                               num_workers = 4,
                                distributed=True
                               )    
 
