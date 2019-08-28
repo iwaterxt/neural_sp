6290824e5adab5bb8d96f5518a6ffea7debafe0d
iwaterxt <iwaterxt@gmail.com>
2019-08-28 11:25:08 +0800

    update

--- neural_sp/bin/asr/train_horovod.py
+++ neural_sp/bin/asr/train_horovod.py
@@ -344,6 +344,8 @@ def main():
                 # Compute loss in the dev set
                 model.eval()
                 batch_dev = dev_set.next()[0]
+                print (type(batch_dev['ys']))
+                print batch_dev['ys']
                 # Change mini-batch depending on task
                 for task in tasks:
                     if skip_thought:
