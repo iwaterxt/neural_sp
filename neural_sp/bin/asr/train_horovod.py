cd71edbfaae6241b6145c2ac590c83de1a3ce447
iwaterxt <iwaterxt@gmail.com>
2019-09-03 16:22:51 +0800

    update

--- neural_sp/bin/asr/train_horovod.py
+++ neural_sp/bin/asr/train_horovod.py
@@ -161,7 +161,7 @@ def main():
                                  num_stacks=args.n_stacks,
                                  num_splices=args.n_splices,
                                  num_skips=args.n_skips,
-                                 pin_memory=True
+                                 pin_memory=False
                                 )
     val_loader = SeqDataloader(dev_set, 
                                batch_size=args.batch_size,
@@ -170,7 +170,7 @@ def main():
                                num_stacks=args.n_stacks,
                                num_splices=args.n_splices,
                                num_skips=args.n_skips,
-                               pin_memory=True
+                               pin_memory=False
                               )    
 
     # Load a LM conf file for LM fusion & LM initialization
