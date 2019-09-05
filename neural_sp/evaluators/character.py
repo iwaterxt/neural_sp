203d341d4be9c1e8a18291a634e57e9aef2414f6
iwaterxt <iwaterxt@gmail.com>
2019-09-05 17:32:47 +0800

    update

--- neural_sp/evaluators/character.py
+++ neural_sp/evaluators/character.py
@@ -88,7 +88,7 @@ def eval_char(models, dataset, recog_params, epoch,
             for b in range(len(batch['xs'])):
                 ref = batch['text'][b]
                 hyp = dataset.idx2token[task_idx](best_hyps_id[b])
-                print (hpy)
+                print (hyp)
                 print (best_hyps_id[b])
                 # Write to trn
                 utt_id = str(batch['utt_ids'][b])
