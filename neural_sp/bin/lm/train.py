d9d4e1d75edabe6f679a48680c809195375a075a
iwaterxt <iwaterxt@gmail.com>
2019-09-06 10:00:00 +0800

    update

--- neural_sp/bin/lm/train.py
+++ neural_sp/bin/lm/train.py
@@ -200,6 +200,7 @@ def main():
     while True:
         # Compute loss in the training set
         ys_train, is_new_epoch = train_set.next()
+        print (ys_train)
         accum_n_tokens += sum([len(y) for y in ys_train])
         optimizer.zero_grad()
         loss, hidden, reporter = model(ys_train, hidden, reporter)
