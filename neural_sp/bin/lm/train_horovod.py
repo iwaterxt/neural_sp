1dd3bd3538655e5428ab485598cdadf54e289e84
iwaterxt <iwaterxt@gmail.com>
2019-09-06 11:50:49 +0800

    update

--- neural_sp/bin/lm/train_horovod.py
+++ neural_sp/bin/lm/train_horovod.py
@@ -281,6 +281,7 @@ def main():
                                     np.exp(loss_train), np.exp(loss_dev),
                                     optimizer.lr, ys_train.shape[0], duration_step / 60))
                     start_time_step = time.time()
+                print (ys_train)
                 pbar_epoch.update(ys_train.shape[0] * (ys_train.shape[1] - 1))
 
             # Save fugures of loss and accuracy
