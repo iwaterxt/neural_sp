dd8599bb7173bab8961e8ee06b46f1009680bd82
iwaterxt <iwaterxt@gmail.com>
2019-09-06 15:55:27 +0800

    update

--- neural_sp/datasets/lm.py
+++ neural_sp/datasets/lm.py
@@ -151,7 +151,7 @@ class Dataset(data.Dataset):
 
         bptt = self.bptt
         print (self.concat_ids.shape[0])
-        print (self.concat_ids[0,929500:929590])
+        print (self.concat_ids[0,929500:929591])
 
         ys = self.concat_ids[:, index*bptt:(index+1)*bptt]
 
