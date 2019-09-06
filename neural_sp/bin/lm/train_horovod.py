861e2897a5e75e133913cecc2e888b8414b1454a
iwaterxt <iwaterxt@gmail.com>
2019-09-06 14:38:52 +0800

    update

--- neural_sp/bin/lm/train_horovod.py
+++ neural_sp/bin/lm/train_horovod.py
@@ -95,12 +95,12 @@ def main():
     args.vocab = train_set.vocab
 
     train_loader = ChunkDataloader(train_set,
-                                   batch_size=args.batch_size,
+                                   batch_size=1,
                                    num_workers = 1,
                                    distributed=True)
 
     val_loader = ChunkDataloader(dev_set,
-                                 batch_size=args.batch_size,
+                                 batch_size=1,
                                  num_workers=1,
                                  distributed=True)
 
