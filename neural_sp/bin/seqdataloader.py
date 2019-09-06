e1ea9754212857e09ac4d66a5b85806e9bf43cc4
iwaterxt <iwaterxt@gmail.com>
2019-09-06 13:16:11 +0800

    update

--- neural_sp/bin/seqdataloader.py
+++ neural_sp/bin/seqdataloader.py
@@ -61,7 +61,7 @@ class ChunkDataloader(DataLoader):
 
         for item in batch:
             for x in item:
-              if x.size == 0:
+              if x.size != 0:
                 y.append(x)
         return np.array(y)
 
