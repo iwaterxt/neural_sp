a9227e903e52b7bbc5c6df7ba3432cd2f29fd84f
iwaterxt <iwaterxt@gmail.com>
2019-09-24 15:15:52 +0800

    update

--- neural_sp/bin/asr/train_horovod.py
+++ neural_sp/bin/asr/train_horovod.py
@@ -221,19 +221,19 @@ def main():
 
 
 
-        
+
         #broadcast
         optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=model.named_parameters())
-        optimizer = LRScheduler(optimizer, conf['lr'],
-                                decay_type=conf['lr_decay_type'],
-                                decay_start_epoch=conf['lr_decay_start_epoch'],
-                                decay_rate=conf['lr_decay_rate'],
-                                decay_patient_n_epochs=conf['lr_decay_patient_n_epochs'],
-                                early_stop_patient_n_epochs=conf['early_stop_patient_n_epochs'],
-                                warmup_start_lr=conf['warmup_start_lr'],
-                                warmup_n_steps=conf['warmup_n_steps'],
-                                model_size=conf['d_model'],
-                                factor=conf['lr_factor'],
+        optimizer = LRScheduler(optimizer, args.lr,
+                                decay_type=args.lr_decay_type,
+                                decay_start_epoch=args.lr_decay_start_epoch,
+                                decay_rate=args.lr_decay_rate,
+                                decay_patient_n_epochs=args.lr_decay_patient_n_epochs,
+                                early_stop_patient_n_epochs=args.early_stop_patient_n_epochs,
+                                warmup_start_lr=args.warmup_start_lr,
+                                warmup_n_steps=args.warmup_n_steps,
+                                model_size=args.d_model,
+                                factor=args.lr_factor,
                                 noam=noam)
         if hvd_rank == 0 :
             # Restore the last saved model
